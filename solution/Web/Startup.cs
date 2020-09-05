e266f50a07c910ee6e92392ef92fce2b84a61893
Abdullah Barrak <a@abarrak.com>
2020-09-05 05:52:02 +0300

    mv

--- solution/Web/Startup.cs
+++ solution/Web/Startup.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -41,11 +40,7 @@ namespace WebUI
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios,
-                // see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
