1466b023dfd7b5c1982ced9fb637023793735176
Abdullah Barrak <a@abarrak.com>
2020-12-07 02:29:01 +0300

    fix migration scripts

--- solution/Persistence/db/migrations/01_initial_script.sql
+++ solution/Persistence/db/migrations/01_initial_script.sql
@@ -8,14 +8,3 @@ CREATE TABLE IF NOT EXISTS MediaFile (
   Description TEXT,
   UploadedAt DATETIME
 );
-
-INSERT OR IGNORE INTO MediaFile VALUES (
-  1,
-  'Sample MP4 File',
-  '.mp4',
-  26800000,
-  '5985d465-4b2c-4c27-966b-96a9a09198db/demo.mp4',
-  '5985d465-4b2c-4c27-966b-96a9a09198db/manifest/manifest.mpd',
-  'A sample MPEG4 served adaptivily by DASH stream',
-  '9/4/2020 10:26:19 PM'
-);
--- solution/Persistence/migrate.sh
+++ solution/Persistence/migrate.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sqlite3 database.db 'CREATE TABLE IF NOT EXISTS MediaFile ( Id INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(100) NOT NULL, Type VARCHAR(50) NOT NULL, Size DECIMAL(10, 2), FilePath TEXT NOT NULL, ManifestPath TEXT NOT NULL, Description TEXT, LockForEncoding INTEGER DEFAULT 0, UploadedAt DATETIME);' '.exit'
+sqlite3 database.db '.exit'
+
+sqlite3 database.db < 01_initial_script.sql
+sqlite3 database.db < 02_add_locking_column_to_mediafile_table.sql
