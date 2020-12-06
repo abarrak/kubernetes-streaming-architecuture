8773a82dce35df2693359d0a14172d51d5d3997d
Abdullah Barrak <a@abarrak.com>
2020-12-07 01:56:06 +0300

    another fix

--- solution/Persistence/migrate.sh
+++ solution/Persistence/migrate.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-sqlite3 database.db 'CREATE TABLE IF NOT EXISTS MediaFile (
-  Id INTEGER PRIMARY KEY AUTOINCREMENT,
-  Name VARCHAR(100) NOT NULL,
-  Type VARCHAR(50) NOT NULL,
-  Size DECIMAL(10, 2),
-  FilePath TEXT NOT NULL,
-  ManifestPath TEXT NOT NULL,
-  Description TEXT,
-  LockForEncoding INTEGER DEFAULT 0,
-  UploadedAt DATETIME
-);' '.exit'
+sqlite3 database.db 'CREATE TABLE IF NOT EXISTS MediaFile ( Id INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(100) NOT NULL, Type VARCHAR(50) NOT NULL, Size DECIMAL(10, 2), FilePath TEXT NOT NULL, ManifestPath TEXT NOT NULL, Description TEXT, LockForEncoding INTEGER DEFAULT 0, UploadedAt DATETIME);' '.exit'
